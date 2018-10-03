239bcba68facfa9a2ec80d4ed08407e0e51721c2
Vlad Dulau <vlad.dulau@amdaris.com>
2018-10-03 15:48:19 +0300

    Added Services

---
+++ MarsRover/Services/RoverController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MarsRover.Domain;
+using MarsRover.Strategies;
+
+namespace MarsRover.Services
+{
+    public class RoverController
+    {
+        private Rover _rover;
+        private readonly IRoverMovementStrategy _roverMovementStrategy;
+        private readonly IEnumerable<IEnumerable<Coordinates>> _map;
+
+        public RoverController(Rover rover,
+                               IRoverMovementStrategy roverMovementStrategy,
+                               IEnumerable<IEnumerable<Coordinates>> map)
+        {
+            this._rover = rover;
+            this._roverMovementStrategy = roverMovementStrategy;
+            this._map = map;
+        }
+        public void ChangeRover(Rover rover) =>
+            this._rover = rover;
+
+        public void Move(IEnumerable<char> commands)
+        {
+            this._roverMovementStrategy.MoveRover(commands);
+        }
+    }
+}
