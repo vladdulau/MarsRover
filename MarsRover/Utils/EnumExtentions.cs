19e52a5f3777e52f8ef7135bebb0cff257fd5e5e
Vlad Dulau <vlad.dulau@amdaris.com>
2018-10-03 15:49:31 +0300

    Added Utils

---
+++ MarsRover/Utils/EnumExtentions.cs
@@ -0,0 +1,36 @@
+﻿using MarsRover.Domain;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MarsRover.Utils
+{
+    public static class EnumExtentions
+    {
+        public static string ToDescription(this Enum en)
+        {
+
+            Type type = en.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+
+            if (memInfo != null && memInfo.Length > 0)
+            {
+
+                object[] attrs = memInfo[0].GetCustomAttributes(
+                                              typeof(DisplayText),
+
+                                              false);
+
+                if (attrs != null && attrs.Length > 0)
+
+                    return ((DisplayText)attrs[0]).Text;
+
+            }
+
+            return en.ToString();
+
+        }
+    }
+}
